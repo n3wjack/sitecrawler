e319d06420803b3815c510f44dec16b62b7cef4d
n3wjack <n3wjack@n3wjack.net>
2019-01-13 14:19:36 +0100

    Get app settings from command line parameters. Add help for console app.

---
+++ Crawler/Configuration/AppSettings.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+
+namespace Crawler.Configuration
+{
+    public class AppSettings
+    {
+        private string[] _args;
+        private string[]_helpSwitches = new string[] { "-h", "/?", "--help", "-help", "-?" };
+
+        public AppSettings(string[] args)
+        {
+            _args = args;
+        }
+
+        public string OutputFile { get; set; }
+        public string Url { get; set; }
+
+        public bool ShowHelp
+        {
+            get { return _args.ToList().Any(s => _helpSwitches.Contains(s.ToLowerInvariant()) ); }
+        }
+
+        public bool IsValid
+        {
+            get
+            {
+                return !string.IsNullOrWhiteSpace(Url);
+            }
+        }
+    }
+}
--- Crawler/Program.cs
+++ Crawler/Program.cs
@@ -1,7 +1,11 @@
 ﻿using Crawler.AppCore;
+using Crawler.Configuration;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace Crawler
@@ -13,13 +17,26 @@ namespace Crawler
 
         static void Main(string[] args)
         {
+            var appSettings = new AppSettings(args);
+            new ConfigurationBuilder().AddCommandLine(args).Build().Bind(appSettings);
+
+            if (!appSettings.IsValid || appSettings.ShowHelp)
+            {
+                if (!appSettings.ShowHelp)
+                {
+                    Console.WriteLine("\nERROR : Invalid or incomplete command line parameters.");
+                }
+
+                ShowHelp();
+                return;
+            }
+
+            Console.WriteLine($"Ready to crawl {appSettings.Url}");
+
             Console.WriteLine("Press ENTER to start crawling.");
             Console.ReadLine();
 
-            //var uri = "https://previewm.janitv.be/";
-            var uri = "https://localhost:44339/";
-
-            var crawler = new WebCrawler(new WebCrawlConfiguration { Uri = new Uri(uri) });
+            var crawler = new WebCrawler(new WebCrawlConfiguration { Uri = new Uri(appSettings.Url) });
             crawler.LinkCrawled += Crawler_LinkCrawled;
 
             Console.CancelKeyPress += (sender, e) => 
@@ -34,9 +51,21 @@ namespace Crawler
             var results = crawler.Start();
 
             Console.WriteLine("Crawling done.");
-            WriteCsv(results);
+            if (!string.IsNullOrWhiteSpace(appSettings.OutputFile))
+            {
+                WriteCsv(results, appSettings.OutputFile);
+            }
 
             Console.ReadLine();
+        }
+
+        private static void ShowHelp()
+        {
+            var assembly = typeof(Program).GetTypeInfo().Assembly;
+            using (var s = new StreamReader(assembly.GetManifestResourceStream("Crawler.Help.txt")))
+            {
+                Console.Write(s.ReadToEnd());
+            }
         }
 
         private static void Crawler_LinkCrawled(object sender, LinkCrawlResult crawlResult)
@@ -49,10 +78,8 @@ namespace Crawler
             Console.WriteLine($"-- Crawled: {crawlResult.Url}\n\t Result : {crawlResult.StatusCode}, Links: {crawlResult.Links.Count} - Total crawled: {_linksCrawled} in {duration} ({linksPerSecond} links/s) ");
         }
 
-        private static void WriteCsv(IList<LinkCrawlResult> results)
+        private static void WriteCsv(IList<LinkCrawlResult> results, string filename)
         {
-            var filename = @".\crawl-result.csv";
-
             Console.WriteLine("Writing to csv file");
             Console.WriteLine($"   Results : {results.Count}");
             results.ToList().ForEach(r => System.IO.File.AppendAllText(filename, $"\"{r.Url}\";{r.StatusCode}\n"));
