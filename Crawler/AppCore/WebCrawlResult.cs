6f424f06102031e5b317f2054ba91a3d80b85012
n3wjack <n3wjack@n3wjack.net>
2020-12-29 11:06:20 +0100

    Remove obsolete class.

--- Crawler/AppCore/WebCrawlResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Crawler.AppCore
-{
-    public class WebCrawlResult
-    {
-        public bool Success { get; set; }
-        
-        // TODO: delete WebCrawlResult or use it.
-    }
-}
