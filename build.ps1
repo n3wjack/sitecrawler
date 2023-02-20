9a5b44f8206e13747a97db498d27d546232940f9
n3wjack <n3wjack@n3wjack.net>
2023-02-20 19:48:02 +0100

    Add project link to help text. Update build script to create packages.

--- build.ps1
+++ build.ps1
@@ -1,8 +1,13 @@
-Write-Host "`n*** Building self-contained winx64 exectable. ***`n" -ForegroundColor Cyan
+param ($version="1.0")
+
+Write-Host "`n*** Building packages. ***`n" -ForegroundColor Cyan
 
 cd Crawler
+dotnet publish -c Release 
 dotnet publish -p:PublishSingleFile=true -r win-x64 -c Release --self-contained true -p:PublishTrimmed=true
 
-cd bin\Release\net6.0\win-x64\publish
+cd ..
+Compress-Archive Crawler\bin\Release\net6.0\publish\*.* Crawler-$version.zip
+Compress-Archive Crawler\bin\Release\net6.0\win-x64\publish\Crawler.exe Crawler-$version-windowsx64-self-contained.zip
 
-ls *.exe
+ls *.zip
